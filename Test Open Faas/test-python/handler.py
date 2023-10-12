32c55fefb3b092aaf2bcb50f3b719404f0562e7f
Valentin CHEREL <valentin.cherel@insa-rennes.fr>
2023-10-12 11:01:28 +0200

    Test communication with NATS in Python

--- Test Open Faas/test-python/handler.py
+++ Test Open Faas/test-python/handler.py
@@ -1,13 +1,33 @@
-import requests
+import json
+import asyncio
+import nats
 
+# Define a function to publish a message to a NATS subject
+async def publish_message(subject, message):
+    # Connect to the NATS server
+    nc = await nats.connect("nats://nats-cluster.default.svc.cluster.local:4222")
+
+    # Publish the message to the specified subject
+    await nc.publish(subject, json.dumps(message).encode())
+
+    # Ensure the message is sent
+    await nc.flush()
+
+    # Close the NATS connection
+    await nc.close()
+
+# Handle the incoming request
 def handle(req):
-    # Replace 'function2-service' with the actual hostname of your second function
-    function2_url = "http://test-python-2:8080"  # Assuming default OpenFaaS port
+    # Create a data payload to be sent as a message
+    data = {"message": "Hello from function1!"}
 
-    # Send an HTTP GET request to function2
-    response = requests.get(function2_url)
+    # Publish the data to a NATS subject named "function1-subject"
+    
+    # Create an event loop to run the asynchronous function
+    loop = asyncio.get_event_loop()
+    
+    # Run the publish_message function asynchronously
+    loop.run_until_complete(publish_message("function1-subject", data))
 
-    if response.status_code == 200:
-        return f"Function 1 called Function 2, and Function 2 responded: {response.text}"
-    else:
-        return "Error: Unable to communicate with Function 2"
+    # Return the data as a JSON response
+    return json.dumps(data)
